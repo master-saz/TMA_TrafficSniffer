aef70515a7dc4a10b5b13e69c701ca01691416f2
master-saz <sebastian.andrade@ithinkupc.com>
2023-10-08 19:18:55 +0200

    update

--- sniffer.c
+++ sniffer.c
@@ -8,12 +8,9 @@
 #include <netinet/if_ether.h>
 
 /*
-the callback function to process our captured packets.
- This function just prints out a running count of packets, as captured. 
- Afterwards we’ll write another callback function.
- The function is very clear, so there’s no need to explain it.
+The callback function processes captured packets.
+This function just prints out a running count of packets, as captured.
 */
-
 void my_callback(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char* 
     packet) 
 { 
@@ -22,6 +19,82 @@ void my_callback(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char*
     fflush(stdout);
     count++; 
 }
+
+void another_callback(u_char *arg, const struct pcap_pkthdr* pkthdr, 
+        const u_char* packet) 
+{ 
+    int i=0; 
+    static int count=0; 
+ 
+    //printf("Packet Count: %d\n", ++count);    /* Number of Packets */
+    //printf("Recieved Packet Size: %d\n", pkthdr->len);    /* Length of header */
+    printf("Recieved Packet TimeStamp: %d\n", pkthdr->ts);    /* Length of header */
+}
+
+void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_char *packetptr)
+{
+    struct ip* iphdr;
+    struct icmp* icmphdr;
+    struct tcphdr* tcphdr;
+    struct udphdr* udphdr;
+    char iphdrInfo[256];
+    char srcip[256];
+    char dstip[256];
+ 
+
+    // Skip the datalink layer header and get the IP header fields.
+    packetptr += linkhdrlen;
+    iphdr = (struct ip*)packetptr;
+    strcpy(srcip, inet_ntoa(iphdr->ip_src));
+    strcpy(dstip, inet_ntoa(iphdr->ip_dst));
+    sprintf(iphdrInfo, "ID:%d TOS:0x%x, TTL:%d IpLen:%d DgLen:%d",
+            ntohs(iphdr->ip_id), iphdr->ip_tos, iphdr->ip_ttl,
+            4*iphdr->ip_hl, ntohs(iphdr->ip_len));
+ 
+    // Advance to the transport layer header then parse and display
+    // the fields based on the type of hearder: tcp, udp or icmp.
+    packetptr += 4*iphdr->ip_hl;
+    switch (iphdr->ip_p)
+    {
+    case IPPROTO_TCP:
+        tcphdr = (struct tcphdr*)packetptr;
+        printf("TCP  %s:%d -> %s:%d\n", srcip, ntohs(tcphdr->th_sport),
+               dstip, ntohs(tcphdr->th_dport));
+        printf("%s\n", iphdrInfo);
+        printf("%c%c%c%c%c%c Seq: 0x%x Ack: 0x%x Win: 0x%x TcpLen: %d\n",
+               (tcphdr->th_flags & TH_URG ? 'U' : '*'),
+               (tcphdr->th_flags & TH_ACK ? 'A' : '*'),
+               (tcphdr->th_flags & TH_PUSH ? 'P' : '*'),
+               (tcphdr->th_flags & TH_RST ? 'R' : '*'),
+               (tcphdr->th_flags & TH_SYN ? 'S' : '*'),
+               (tcphdr->th_flags & TH_SYN ? 'F' : '*'),
+               ntohl(tcphdr->th_seq), ntohl(tcphdr->th_ack),
+               ntohs(tcphdr->th_win), 4*tcphdr->th_off);
+        printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
+        packets += 1;
+        break;
+ 
+    case IPPROTO_UDP:
+        udphdr = (struct udphdr*)packetptr;
+        printf("UDP  %s:%d -> %s:%d\n", srcip, ntohs(udphdr->uh_sport),
+               dstip, ntohs(udphdr->uh_dport));
+        printf("%s\n", iphdrInfo);
+	    printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
+        packets += 1;
+        break;
+ 
+    case IPPROTO_ICMP:
+        icmphdr = (struct icmp*)packetptr;
+        printf("ICMP %s -> %s\n", srcip, dstip);
+        printf("%s\n", iphdrInfo);
+        printf("Type:%d Code:%d ID:%d Seq:%d\n", icmphdr->icmp_type, icmphdr->icmp_code,
+               ntohs(icmphdr->icmp_hun.ih_idseq.icd_id), ntohs(icmphdr->icmp_hun.ih_idseq.icd_seq));
+	    printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
+        packets += 1;
+        break;
+    }
+}
+
 
 int main(int argc,char **argv) 
 { 
@@ -72,6 +145,6 @@ int main(int argc,char **argv)
     } 
  
     /* loop for callback function */
-    pcap_loop(descr, -1, my_callback, NULL); 
+    pcap_loop(descr, -1, packet_handler, NULL); 
     return 0; 
 }
